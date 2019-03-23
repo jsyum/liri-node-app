8b217c72dc82261081b41f2ce461d8e2dd86f3cc
jsyum <jennifersyum@gmail.com>
2019-03-22 23:08:13 -0400

    spotify and imdb working

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
 console.log("this is loaded");
 
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "4c4399ab9a4743098ffd5b6ebc3ffd1d",
+  secret: "9e497454bbe54dcd9815d44a066274c3"
 };
